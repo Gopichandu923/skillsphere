9ffefb768a062899d241cd2a7df19f797a88f4df
chandu <gopichandu6080@gmail.com>
2025-04-11 08:47:38 +0530

    database configuration

---
+++ server/database/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const url = process.env.MONGO_URL_LOCAL;
+
+const connect = () => {
+  mongoose.connect(url).then(() => {
+    console.log("succesfully connected to database");
+  });
+};
+export { connect };
